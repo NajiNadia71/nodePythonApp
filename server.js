d18d9dbd98163f4e4e72d01cab6d4eadc799fa8e
selight <ghiwotselam@gmail.com>
2023-06-25 18:42:17 +0200

    some changes to the server

--- server.js
+++ server.js
@@ -1,38 +1,52 @@
-const http = require('http');
-const fs = require('fs');
-const querystring = require('querystring');
-
-const hostname = 'localhost';
+const express = require('express');
+const { spawn } = require('child_process');
+const app = express();
+const cors = require('cors');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 const port = 3020;
+const fs = require('fs');
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
+app.get("/", (req, res) => {
+    res.send("Welcome to CNL2ASP API, to convert please use /convert endpoint with POST method");
+});
+app.post("/convert", (req, res) => {
+    try {
+        let Input = req.body.input;
+        console.log(req.body, {Input});
+        let largeDataSet = [];
 
-const server = http.createServer((req, res) => {
-    if (req.method === 'POST') {
-        console.log('I Came Here');
-        debugger;
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString();
+        fs.appendFile('src/InputUserFile', Input, function (err) {
+            if (err) throw err;
         });
-        req.on('end', () => {
-            console.log(body);;
-            res.statusCode = 302;
-            res.setHeader('Location', '/');
-            res.end();
+        const python = spawn('python', ['src/main.py', 'src/InputUserFile', 'result.lp']);
+        python.stdout.on('data', function (data) {
+            largeDataSet.push(data);
         });
-    } else {
-        fs.readFile('form_tosend.html', (err, data) => {
-            if (err) {
-                res.statusCode = 500;
-                res.end('Internal Server Error');
-            } else {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'text/html');
-                res.end(data);
-            }
+        python.on('close', () => {
+            fs.readFile('result.lp', 'utf8', (err, data) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                let result = data.toString();
+                let pathFileInput = 'src/InputUserFile';
+                let pathFileResult = 'result.lp';
+                fs.unlink(pathFileInput, (err) => {
+                    if (err) throw err
+                });
+
+                fs.unlink(pathFileResult, (err) => {
+                    if (err) throw err
+                });
+                res.send({Result: result});
+            });
         });
+}
+    catch (err) {
+        return res.status(500).json({ message: err.message });
     }
-});
-
-server.listen(port, hostname, () => {
-    // console.log(Server running at http://${hostname}:${port}/);
-});
+})
